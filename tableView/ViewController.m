8632ed50ce65cb92a0e4cbbdd4a00ae9d41d1631
Andrey Kowalsky <gold9617@gmail.com>
2015-10-16 14:45:25 +0300

    test

--- tableView/ViewController.m
+++ tableView/ViewController.m
@@ -13,15 +13,58 @@
 @end
 
 @implementation ViewController
-
+//fsfsf
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIEdgeInsets inset = UIEdgeInsetsMake(20, 0, 0, 0);
+    
+    self.tableView.contentInset = inset;
+    self.tableView.scrollIndicatorInsets = inset;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 30;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    
+    //UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"aaa"];
+    
+    NSString* indentifier = @"cell";
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:indentifier];
+    
+    if (!cell) {
+        NSLog(@"Cell created");
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:indentifier];
+    }else {
+        NSLog(@"Cell reused");
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"section = %ld row = %ld", indexPath.section, indexPath.row];
+    
+    return cell;
+    
+}
+
 
 @end
